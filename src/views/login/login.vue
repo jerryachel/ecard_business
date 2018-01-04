06faa6425b9afcfb15ee77e8941dea6005dc9473
jerryachel <jerryachel@foxmail.com>
2018-01-04 16:43:54 +0800

    搭建环境

---
+++ src/views/login/login.vue
@@ -0,0 +1,32 @@
+<template>
+	<div class="login">
+		<div class="nav">
+			<img class="logo" src="../../assets/images/logo.png" alt="">
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	data () {
+		return {
+			msg: ''
+		}
+	}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+.login{
+	.nav{
+		width: 100%;
+		height: 90px;
+		display: flex;
+		align-items:center;
+	}
+	.logo{
+		width: 280px;
+	}
+}
+</style>
