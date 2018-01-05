8a70b0e37f2739bda1965541afee15e135bf804a
jerryachel <jerryachel@foxmail.com>
2018-01-05 17:03:13 +0800

    登录页面title

---
+++ src/views/login/header.vue
@@ -0,0 +1,30 @@
+<template>
+	<div class="login_nav">
+		<img class="logo" src="../../assets/images/logo.png" alt="">
+		<div class="nav_btn">
+			<router-link to="/login">登录</router-link>
+			<router-link to="/register">注册</router-link>
+			<a href="#">顾客登录/注册</a>
+		</div>
+	</div>
+</template>
+<script>
+	
+</script>
+<style lang="scss" >
+@import '../../assets/css/index.scss';
+.login_nav{
+	width: 100%;
+	height: 100px;
+	@extend %flex_justify;
+	color: #333;
+	padding: 10px 50px;
+	.logo{
+		width: 180px;
+	}
+	a{
+		margin: 0 20px;
+		font-size: 18px;
+	}
+}
+</style>
--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,17 +1,19 @@
 <template>
 	<div class="login">
-		<div class="nav">
-			<img class="logo" src="../../assets/images/logo.png" alt="">
-		</div>
+		<login-nav></login-nav>
 	</div>
 </template>
 
 <script>
+import loginNav from './header.vue'
 export default {
 	data () {
 		return {
 			msg: ''
 		}
+	},
+	components:{
+		loginNav:loginNav
 	}
 }
 </script>
@@ -19,14 +21,6 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss">
 .login{
-	.nav{
-		width: 100%;
-		height: 90px;
-		display: flex;
-		align-items:center;
-	}
-	.logo{
-		width: 280px;
-	}
+
 }
 </style>
